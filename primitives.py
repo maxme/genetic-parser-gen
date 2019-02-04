4dfd7c28be7931e3b12be330aade94452f6a8e08
Maxime Biais <maxime.biais@gmail.com>
2019-02-04 17:09:02 +0100

    Add new replacement primitives

--- primitives.py
+++ primitives.py
@@ -1,18 +1,29 @@
-from itertools import ifilter
 import re
 
 # string primitives
 def str_remove_tabs(s):
     return s.replace("\t", " ")
 
+
 def str_remove_endline(s):
     return s.replace("\n", " ")
 
+
 def str_remove_consecutive_spaces(s):
-    return re.sub(r'\ +', ' ', s)
+    return re.sub(r"\ +", " ", s)
+
 
 def str_strip(s):
     return s.strip()
+
+
+def str_replace_comma(s):
+    return s.replace(",", " ")
+
+
+def str_replace_whitespace(s):
+    return s.replace(" ", ",")
+
 
 # regexp primitives
 s2l_date1 = ("([0-9]{2}/[0-9]{2})", 1)
@@ -22,7 +33,10 @@ s2l_float = ("(-?[0-9]{1,20}[.,]+[0-9]{1,20})", 1)
 s2l_int = ("(-?[0-9]{1,20})", 1)
 
 s2l_everything_nongreedy = ("(.{1,100}?)", 0)
-s2l_everything_nongreedy_remove = (".{1,100}?", 0) # arbitrary chosen 100 limit because of : http://www.regular-expressions.info/catastrophic.html
+s2l_everything_nongreedy_remove = (
+    ".{1,100}?",
+    0,
+)  # arbitrary chosen 100 limit because of : http://www.regular-expressions.info/catastrophic.html
 
 s2l_endline = ("(\n)", 1)
 s2l_endline_remove = ("\n", 1)
@@ -32,7 +46,6 @@ s2l_whitespace_remove = ("\s", 1)
 
 s2l_seps = ("(['\"\-+,;:\. ])", 1)
 s2l_seps_remove = ("['\"\-+,;:\. ]", 1)
-
 
 # clean primitives
 def clean_strip(l):
@@ -44,12 +57,14 @@ def clean_strip(l):
         l[n] = elt.strip()
     return l
 
+
 def clean_empty(l):
     """
     >>> clean_empty(["a", "", "c"])
     ['a', 'c']
     """
-    return list(ifilter(lambda x: x!= "", l))
+    return list(filter(lambda x: x != "", l))
+
 
 def merge(n=1):
     """
@@ -60,24 +75,26 @@ def merge(n=1):
     >>> merge(3)(["a", "b", "c"])
     ['a', 'b', 'c']
     """
+
     def _in(l):
-        if len(l) < n+1:
+        if len(l) < n + 1:
             return l
-        l[n-1] += l.pop(n)
+        l[n - 1] += l.pop(n)
         return l
-    _in.__name__ = "clean_merge%d" % n
+
     return _in
 
 
 for i in range(1, 10):
-    globals()["clean_merge%d" % i] = merge(i)
+    locals()["clean_merge%d" % i] = merge(i)
 
-str_primitives = [eval(i) for i in ifilter(lambda x: x.startswith("str"),
-                                           dir())]
-clean_primitives = [eval(i) for i in ifilter(lambda x: x.startswith("clean"),
-                                             dir())]
-re_primitives = [eval(i) for i in ifilter(lambda x: x.startswith("s2l"), dir())]
+
+str_primitives = [eval(i) for i in filter(lambda x: x.startswith("str"), dir())]
+clean_primitives = [eval(i) for i in filter(lambda x: x.startswith("clean"), dir())]
+re_primitives = [eval(i) for i in filter(lambda x: x.startswith("s2l"), dir())]
 
 if __name__ == "__main__":
-    from doctest import testmod
-    testmod()
+    # python3 primitives.py -v
+    import doctest
+
+    doctest.testmod()
